162a83802bd4cf66720f34c7123f050867f18e8a
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-03-18 20:46:30 +0530

    Update GenreFilter.js

--- src/components/GenreFilter.js
+++ src/components/GenreFilter.js
@@ -6,6 +6,9 @@ const GenreFilter = ({ genres, onGenreSelect }) => {
   return (
     // printing using map function on here with
     // onclick function 
+    //the key=genre should be used after map function to efficently update
+    //render etc etc.
+    //the key prop needs to be set to a unquie key as mentioned.
     <div className='genre-filter'>
       <h2>Filter by Genre:</h2>
       {genres.map((genre) => (
